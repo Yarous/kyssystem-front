9ff9b181318b43961e1aea109ef8770115fac201
Yarous <slavluch10@gmail.com>
2024-07-11 04:29:47 +0300

    Added a loading animation.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import CardBody from './common/CardBody/CardBody'
 import { useState } from 'react'
 import Header from './components/header/Header'
 import './styles/main.css'
-import CardBody from './common/CardBody/CardBody'
+import './styles/main.css'
+import Loading from './common/Loading/Loading'
 
 function App() {
 	// Указываем тип состояния theme как 'light' | 'dark'
@@ -11,17 +13,12 @@ function App() {
 		setTheme(theme === 'light' ? 'dark' : 'light')
 		document.documentElement.className = theme === 'light' ? 'dark' : 'light'
 	}
-
+	console.log('App is running!')
 	return (
 		<div className='App'>
 			<Header theme={theme} toggleTheme={toggleTheme} />
 			<CardBody theme={theme}>
-				<p>
-					In adipisicing esse cillum amet anim. Ex anim deserunt aliquip in
-					dolor minim eiusmod. Ex ex incididunt tempor et ea dolore deserunt
-					adipisicing ea excepteur commodo sit. Ullamco enim irure non velit
-					consequat nisi sunt enim enim ea.
-				</p>
+				<p>HellO!</p>
 			</CardBody>
 		</div>
 	)
